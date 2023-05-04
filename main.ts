b1b68a2ba6451894514253dcae1114609d5c1abe
fnavizq355 <132046114+fnavizq355@users.noreply.github.com>
2023-05-04 10:32:52 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,13 +5,5 @@ for (let index = 0; index < 4; index++) {
     basic.showIcon(IconNames.SmallHeart)
     basic.pause(100)
 }
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . # . .
-    # . . . #
-    . # # # .
-    `)
-basic.pause(1000)
 basic.clearScreen()
 basic.showString("¡Adios!")
